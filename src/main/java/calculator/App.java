cff809d59daed2f8ba466c3486fdd3c0c1efcc38
Robbie <robbie331@>
2024-07-24 15:00:28 +0900

    영상 두번째 commit

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -12,6 +12,10 @@ public class App {
         System.out.println("두 번째 숫자를 입력하세요: ");
         int num2 = sc.nextInt();
 
-        System.out.println("num1, num2 = " + num1 + ", " + num2);
+        System.out.println("사칙연산 기호를 입력하세요: "); // +, -, *, /
+        char operator = sc.next().charAt(0);
+
+        System.out.println("operator = " + operator);
+
     }
 }
