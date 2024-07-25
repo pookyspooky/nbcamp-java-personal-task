845a86a1b139e0fe0fbda60faa95771aa3cdfcbb
Robbie <robbie331@>
2024-07-25 22:06:44 +0900

    4. 반복문

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -7,33 +7,42 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("첫 번째 숫자를 입력하세요: ");
-        int num1 = sc.nextInt();
-        System.out.println("사칙연산 기호를 입력하세요: "); // +, -, *, /
-        String operator = sc.next();
-        System.out.println("두 번째 숫자를 입력하세요: ");
-        int num2 = sc.nextInt();
+        while (true) {
+            System.out.println("첫 번째 숫자를 입력하세요: ");
+            int num1 = sc.nextInt();
+            System.out.println("사칙연산 기호를 입력하세요: "); // +, -, *, /
+            String operator = sc.next();
+            System.out.println("두 번째 숫자를 입력하세요: ");
+            int num2 = sc.nextInt();
 
-        int result = 0; // 결과 값을 저장할 변수
+            int result = 0; // 결과 값을 저장할 변수
 
-        if (operator.equals("+")) {
-            result = num1 + num2;
-        } else if (operator.equals("-")) {
-            result = num1 - num2;
-        } else if (operator.equals("*")) {
-            result = num1 * num2;
-        } else if (operator.equals("/")) {
-            if (num2 != 0) {
-                result = num1 / num2;
+            if (operator.equals("+")) {
+                result = num1 + num2;
+            } else if (operator.equals("-")) {
+                result = num1 - num2;
+            } else if (operator.equals("*")) {
+                result = num1 * num2;
+            } else if (operator.equals("/")) {
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("오류: 나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                }
             } else {
-                System.out.println("오류: 나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                System.out.println("오류: 올바른 연산 기호를 입력하세요 (+, -, *, / 중 하나).");
             }
-        } else {
-            System.out.println("오류: 올바른 연산 기호를 입력하세요 (+, -, *, / 중 하나).");
+
+
+            System.out.println("결과: " + result);
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String exit = sc.next();
+            if (exit.equals("exit")) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }
         }
-
-        System.out.println("결과: " + result);
-
 
     }
 }
