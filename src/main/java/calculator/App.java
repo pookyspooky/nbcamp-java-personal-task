31f072496cc4a2d96a6cb0dda21c68d2c3295547
Robbie <robbie331@>
2024-07-26 00:20:03 +0900

    5. count

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,11 +1,16 @@
 package calculator;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        int[] intArray = new int[10]; // 크기가 10인 int 배열 생성
+        int count = 0; // 인덱스를 표현하는 언어
+        intArray[count] = 1; // 인덱스 0번자리에 1 저장
 
         while (true) {
             System.out.println("첫 번째 숫자를 입력하세요: ");
@@ -28,13 +33,18 @@ public class App {
                     result = num1 / num2;
                 } else {
                     System.out.println("오류: 나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    continue;
                 }
             } else {
                 System.out.println("오류: 올바른 연산 기호를 입력하세요 (+, -, *, / 중 하나).");
+                continue;
             }
 
-
             System.out.println("결과: " + result);
+
+            intArray[count] = 1;
+            count++;
+            System.out.println(count);
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             String exit = sc.next();
