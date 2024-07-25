defb563e166606a4e67e99cb831702f3661d2bc2
Robbie <robbie331@>
2024-07-25 11:28:14 +0900

    3. 결과값 출력

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -9,13 +9,31 @@ public class App {
 
         System.out.println("첫 번째 숫자를 입력하세요: ");
         int num1 = sc.nextInt();
+        System.out.println("사칙연산 기호를 입력하세요: "); // +, -, *, /
+        String operator = sc.next();
         System.out.println("두 번째 숫자를 입력하세요: ");
         int num2 = sc.nextInt();
 
-        System.out.println("사칙연산 기호를 입력하세요: "); // +, -, *, /
-        char operator = sc.next().charAt(0);
+        int result = 0; // 결과 값을 저장할 변수
 
-        System.out.println("operator = " + operator);
+        if (operator.equals("+")) {
+            result = num1 + num2;
+        } else if (operator.equals("-")) {
+            result = num1 - num2;
+        } else if (operator.equals("*")) {
+            result = num1 * num2;
+        } else if (operator.equals("/")) {
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                System.out.println("오류: 나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+            }
+        } else {
+            System.out.println("오류: 올바른 연산 기호를 입력하세요 (+, -, *, / 중 하나).");
+        }
+
+        System.out.println("결과: " + result);
+
 
     }
 }
